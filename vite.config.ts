849252435eb013cb086010821c843d871f3f09f3
Tasheela Jayawickrama <tasheelajay1999@gmail.com>
2026-09-02 12:17:02 +0530

    chore: configure vite build to split vendor chunks and increase warning limit

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,16 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
